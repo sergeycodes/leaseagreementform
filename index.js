6fae3c5a565f453a874de1cfd15890493f488566
sergeybarzul <96749365+sergeycodes@users.noreply.github.com>
2022-06-15 14:09:06 -0700

    finished ui for first page

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+let cards = document.getElementById("card")
+let cardsEl = document.querySelectorAll(".card")
+
 function move() {
     var elem = document.getElementById("myBar")
-    var width = 1
-    var id = setInterval(frame, 10)
-    function frame() {
-        if(width >= 100) {
-            clearInterval(id)
-        }else {
-            width ++
-            elem.style.width = width + '%'
-        }
-    }
+    elem.style.width = 35 + '%'
 }
+
+cardsEl.addEventListener('click', borderBox, false)
+
+function borderBox() {
+    console.log("clicked")
+}
+
+focusMethod = function getFocus() {
+    document.getElementById("myTextField").focus();
+  }
