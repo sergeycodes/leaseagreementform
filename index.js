74f116052646df33bb50fa59e4c49b19f97dbc4e
sergeybarzul <96749365+sergeycodes@users.noreply.github.com>
2022-06-15 20:53:43 -0700

    started next question

--- index.js
+++ index.js
@@ -1,15 +1,58 @@
-let cards = document.getElementById("card")
-let cardsEl = document.querySelectorAll(".card")
+const optionBox = document.querySelector('#option-box')
+const question = document.querySelector('#top-question')
+const home = document.getElementById('home')
+const apartment = document.getElementById('apartment')
+const condo = document.getElementById('condo')
+const room = document.getElementById('room')
+
+const cards = document.querySelectorAll('card')
+
+const continueBtn = document.querySelector('.continue-btn')
+
+continueBtn.addEventListener('click', move)
 
 function move() {
     var elem = document.getElementById("myBar")
     elem.style.width = 35 + '%'
+    optionBox.style.display = "none"
+    question.innerHTML = "What type of lease term?"
+
+
 }
 
-cardsEl.addEventListener('click', borderBox, false)
+home.addEventListener('click', homeBorderBox)
+apartment.addEventListener('click', apartmentBorderBox)
+condo.addEventListener('click', condoBorderBox)
+room.addEventListener('click', roomBorderBox)
 
-function borderBox() {
-    console.log("clicked")
+
+
+function homeBorderBox() {
+    home.style.border = "5px solid green"
+    apartment.style.border = "none"
+    condo.style.border = "none"
+    room.style.border = "none"
+}
+
+function apartmentBorderBox() {
+    home.style.border = "none"
+    apartment.style.border = "5px solid green"
+    condo.style.border = "none"
+    room.style.border = "none"
+}
+
+function condoBorderBox() {
+    home.style.border = "none"
+    apartment.style.border = "none"
+    condo.style.border = "5px solid green"
+    room.style.border = "none"
+}
+
+function roomBorderBox() {
+    home.style.border = "none"
+    apartment.style.border = "none"
+    condo.style.border = "none"
+    room.style.border = "5px solid green"
 }
 
 focusMethod = function getFocus() {
