aef20ca349bf42d21c71bc0891641c65c67e656a
sergeybarzul <96749365+sergeycodes@users.noreply.github.com>
2022-06-17 13:48:29 -0700

    finished lease form and started details form

--- index.js
+++ index.js
@@ -5,25 +5,30 @@ const condo = document.getElementById('condo')
 const room = document.getElementById('room')
 const continueBtn = document.querySelector('.continue-btn')
 continueBtn.addEventListener('click', firstContinue)
-
 const leaseEl = document.querySelector('.leaseterm')
 
-function firstContinue() {
-    var elem = document.getElementById("myBar")
-    elem.style.width = 35 + '%'
-    propertyEl.style.display = "none" 
-    leaseEl.style.display = "flex"
 
+
+function firstContinue() {
+    
+        var elem = document.getElementById("myBar")
+        elem.style.width = 35 + '%'
+        propertyEl.style.display = "none" 
+        leaseEl.style.display = "flex"
+    
+   
 }
 
 home.addEventListener('click', homeBorderBox)
 apartment.addEventListener('click', apartmentBorderBox)
 condo.addEventListener('click', condoBorderBox)
 room.addEventListener('click', roomBorderBox)
+home.style.border = "5px solid green"
 let selectedProperty = "" 
 
 function homeBorderBox() {
-    home.style.border = "5px solid green"
+    
+    home.style.border = "4px solid green"
     apartment.style.border = "none"
     condo.style.border = "none"
     room.style.border = "none"
@@ -32,7 +37,7 @@ function homeBorderBox() {
 
 function apartmentBorderBox() {
     home.style.border = "none"
-    apartment.style.border = "5px solid green"
+    apartment.style.border = "4px solid green"
     condo.style.border = "none"
     room.style.border = "none"
     selectedProperty = "apartment"
@@ -41,7 +46,7 @@ function apartmentBorderBox() {
 function condoBorderBox() {
     home.style.border = "none"
     apartment.style.border = "none"
-    condo.style.border = "5px solid green"
+    condo.style.border = "4px solid green"
     room.style.border = "none"
     selectedProperty = "condo"
 }
@@ -50,40 +55,49 @@ function roomBorderBox() {
     home.style.border = "none"
     apartment.style.border = "none"
     condo.style.border = "none"
-    room.style.border = "5px solid green"
+    room.style.border = "4px solid green"
     selectedProperty = "room"
 }
+
 
 //lease js
 
 const fixedDate = document.querySelector('#fixed-date')
 const month = document.querySelector('#month') 
 const year = document.querySelector('#year') 
+const startDate = document.querySelector('#start-date')
+const endDate = document.querySelector('#end-date')
 
 fixedDate.addEventListener('click', fixedDateBorderBox)
 month.addEventListener('click', monthBorderBox)
 year.addEventListener('click', yearBorderBox)
 let selectedLease = "" 
 
+
+fixedDate.style.border = "5px solid green"
+
 function fixedDateBorderBox() {
-    fixedDate.style.border = "5px solid green"
+    fixedDate.style.border = "4px solid green"
     month.style.border = "none"
     year.style.border = "none"
     selectedLease = "fixedDate"
+    endDate.style.display = "grid"
 }
 
 function monthBorderBox() {
     fixedDate.style.border = "none"
-    month.style.border = "5px solid green"
+    month.style.border = "4px solid green"
     year.style.border = "none"
     selectedLease = "month"
+    endDate.style.display = "none"
 }
 
 function yearBorderBox() {
     fixedDate.style.border = "none"
     month.style.border = "none"
-    year.style.border = "5px solid green"
+    year.style.border = "4px solid green"
     selectedLease = "year"
+    endDate.style.display = "none"
 }
 
 const backBtn = document.querySelector('.back-btn')
@@ -95,3 +109,7 @@ function backButton() {
     var elem = document.getElementById("myBar")
     elem.style.width = 5 + '%'
 }
+
+const continueTwoBtn = document.querySelector('#continue-two')
+
+//details js
